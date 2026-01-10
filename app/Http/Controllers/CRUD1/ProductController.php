9fe4910fb5a3d34c71f8d6e3d60d2d657c97b317
azizhadiid <azizalhadiid88@gmail.com>
2026-01-10 20:57:25 +0700

    Start Route and Controllers

--- app/Http/Controllers/CRUD1/ProductController.php
+++ app/Http/Controllers/CRUD1/ProductController.php
@@ -4,8 +4,55 @@ namespace App\Http\Controllers\CRUD1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = DB::table('products')->get();
+        return view('products.index', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        DB::table('products')->insert([
+            'name' => $request->name,
+            'price' => $request->price,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        return redirect('/products');
+    }
+
+    public function edit($id)
+    {
+        $product = DB::table('products')->where('id', $id)->first();
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        DB::table('products')
+            ->where('id', $id)
+            ->update([
+                'name' => $request->name,
+                'price' => $request->price,
+                'updated_at' => now(),
+            ]);
+
+        return redirect('/products');
+    }
+
+    public function destroy($id)
+    {
+        DB::table('products')->where('id', $id)->delete();
+        return redirect('/products');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CRUD1\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,11 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return view('test');
 });
+
+// CRUD 1
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/create', [ProductController::class, 'create']);
+Route::post('/products/store', [ProductController::class, 'store']);
+Route::get('/products/edit/{id}', [ProductController::class, 'edit']);
+Route::post('/products/update/{id}', [ProductController::class, 'update']);
+Route::get('/products/delete/{id}', [ProductController::class, 'destroy']);
